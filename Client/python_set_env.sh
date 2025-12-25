446b7c287ebf9948f4a38a7c6933012247eec5bc
Jesus Acosta <jesusacostagomez@yahoo.com>
2025-12-25 13:57:30 -0600

    Added commands to set virtual enviroment  and to generate stubs files

--- Client/python_set_env.sh
+++ Client/python_set_env.sh
@@ -1,1 +1,5 @@
-python -m grpc_tools.protoc --proto_path=.. --python_out=. --grpc_python_out=. ../service.proto 
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+python3 -m grpc_tools.protoc --proto_path=.. --python_out=. --grpc_python_out=. ../service.proto 
